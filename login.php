8fce48c82107214327e97dbe4c7985ca788e0efb
Aloecheverria7 <aloecheverria7@yahoo.com>
2024-02-13 09:38:16 -0600

    ko

--- login.php
+++ login.php
@@ -1,73 +1,89 @@
 <?php session_start() ?>
 <style>
-   .contact-area {
-    position: relative;
-}
+    .contact-area {
+        position: relative;
+    }
 
-.default-padding {
-    padding-top: 120px;
-    padding-bottom: 120px;
-}
+    .R-forfotPassword{
+        text-align: center;
+        color: #000
+    }
 
-@media (min-width: 1200px) {
+    .bottons-r{
+        display: flex;
+        justify-content: space-between;
+        margin-top: -60px;
+    }
+
+    .default-padding {
+        padding-top: 120px;
+        padding-bottom: 120px;
+    }
+
+    .btn-roger {
+        width: 193px;
+    }
+
+    @media (min-width: 1200px) {
+        .container-full {
+            max-width: 1400px;
+            width: 90%;
+        }
+    }
+
+    .align-center {
+        align-items: center;
+    }
+
+    .row {
+        display: -ms-flexbox;
+        display: flex;
+        -ms-flex-wrap: wrap;
+        flex-wrap: wrap;
+        margin-right: -15px;
+        margin-left: -15px;
+    }
+
+    @media (min-width: 992px) {
+        .col-lg-5 {
+            -ms-flex: 0 0 41.666667%;
+            flex: 0 0 41.666667%;
+            max-width: 41.666667%;
+        }
+    }
+
+    .contact-area .form-content {
+        padding: 90px;
+        background: #ffffff;
+        box-shadow: 0 0 10px #cccccc;
+        position: relative;
+        z-index: 1;
+    }
+
+    .contact-area .form-content h2 {
+        font-weight: 600;
+        margin-bottom: 30px;
+        text-align: center;
+    }
+
+    .form-content {
+        padding: 50px;
+        background: #ffffff;
+        box-shadow: 0 0 10px #cccccc;
+        position: relative;
+        z-index: 1;
+    }
+
     .container-full {
         max-width: 1400px;
         width: 90%;
+        margin: 0 auto;
+        /* Para centrar el contenedor */
     }
-}
 
-.align-center {
-    align-items: center;
-}
-
-.row {
-    display: -ms-flexbox;
-    display: flex;
-    -ms-flex-wrap: wrap;
-    flex-wrap: wrap;
-    margin-right: -15px;
-    margin-left: -15px;
-}
-
-@media (min-width: 992px) {
-    .col-lg-5 {
-        -ms-flex: 0 0 41.666667%;
-        flex: 0 0 41.666667%;
-        max-width: 41.666667%;
+    .align-center {
+        align-items: center;
     }
-}
-
-.contact-area .form-content {
-    padding: 90px;
-    background: #ffffff;
-    box-shadow: 0 0 10px #cccccc;
-    position: relative;
-    z-index: 1;
-}
-
-.contact-area .form-content h2 {
-    font-weight: 600;
-    margin-bottom: 30px;
-    text-align: center;
-}
-
-.form-content {
-    padding: 50px;
-    background: #ffffff;
-    box-shadow: 0 0 10px #cccccc;
-    position: relative;
-    z-index: 1;
-}
-
-.container-full {
-    max-width: 1400px;
-    width: 90%;
-    margin: 0 auto; /* Para centrar el contenedor */
-}
-
-.align-center {
-    align-items: center;
-}
 </style>
 <?php include('php/secction/header.php') ?>
 
@@ -89,15 +105,22 @@
                             </div>
 
                             <div class="form-group">
-                                <input class="form-control" id="password" name="password" placeholder="password" type="password">
+                                <input class="form-control" id="password" name="password" placeholder="password"
+                                    type="password">
                                 <span class="alert-error"></span>
                             </div>
-                            
+
                             <div class="form-group">
-                                <button type="submit" name="btningresar" class="btn btn-primary" value="INICIAR SECION">Login</button>
+                                <button type="submit" name="btningresar" class="btn btn-primary btn-roger"
+                                    value="INICIAR SECION">LOGIN</button>
                             </div>
-                            <div class="form-group">
-                                <button class="btn btn-primary" type="button" onclick="mostrarContrasena()">Restablecer Contraseña</button>
+                            <br> <br>
+                            <div class="col">
+                                <!-- <button class="btn btn-primary" type="button" onclick="mostrarContrasena()">Mostrar Contraseña</button> -->
+                            </div>
+                            <div class="bottons-r">
+                                <a class="R-forfotPassword" href="passwordreset.php">Forgot Password ?</a>
+                                <a class="R-forfotPassword" href="singup.php">Sing Up ?</a>
                             </div>
                             <div class="col-md-12 alert-notification">
                                 <div id="message" class="alert-msg"></div>
